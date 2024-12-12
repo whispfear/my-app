f8713582451611315c6971b6363793e210fcfda4
whispfear <whispfear@gmail.com>
2024-12-12 16:23:10 +0200

    imfckd

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return (
-        <div>
-            <h1>Hello, React!</h1>
-            <p>blah blah</p>
-        </div>
-    );
-};
+const App = () => (
+    <div>
+        <h1>Hello, React!</h1>
+        <p>blah blah</p>
+    </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
