13e539e52260b2e8bee0287fcc5a4323247082af
whispfear <whispfear@gmail.com>
2024-12-12 15:24:25 +0200

    523523523

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h2>blah blah</h2>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('test1'));
