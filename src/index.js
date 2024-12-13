3c269d2da11511f1b2d68ba88054cc9cbd7a419e
whispfear <whispfear@gmail.com>
2024-12-13 10:52:34 +0200

    rem_build1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import VideoList from './VideoList';
+//import VideoList from './VideoList';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
