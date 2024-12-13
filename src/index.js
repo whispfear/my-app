bffd8baf97d04d216d549cb7933c46f4b00e3a02
whispfear <whispfear@gmail.com>
2024-12-13 09:55:35 +0200

    c3y564uv53vb

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Video from './Video';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Video />
   </React.StrictMode>
 );
 
