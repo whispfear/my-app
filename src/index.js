f212660abbf2ce532b2a89ae30eacaf564a2df65
whispfear <whispfear@gmail.com>
2024-12-13 15:48:03 +0200

    renrw_1

--- src/index.js
+++ src/index.js
@@ -5,17 +5,46 @@ import logo from './logo.svg';
 import VideoList from './VideoList';
 import reportWebVitals from './reportWebVitals';
 
-const containerRoot = document.getElementById('root'); 
-const root = createRoot(containerRoot); 
-root.render(<p>Just for test</p>);
+createRoot(document.getElementById('root')).render(
+  <section>
+    <img src={logo} alt="logo" />
+    <p>Just for test</p>
+    <ul>
+      <li>転ぜクば社題ば達全リケ閣壊ヤマヱ相楽</li>
+      <li>せぽ俳部ヒケチ社郎表討賢コネナ専用ウオイ高飛士ふねちよ</li>
+      <li>厚集もほきす読2抗設連びゅの増材</li>
+    </ul>
+  </section>
+)
 
-const containerTest = document.getElementById('test'); 
-const test = createRoot(containerTest); 
-test.render(<ul>
-<li>Text 1</li>
-<li>Text 2</li>
-<li>Text 3</li>
-</ul>)
+createRoot(document.getElementById('test')).render(
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>City</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John Doe</td>
+        <td>30</td>
+        <td>New York</td>
+      </tr>
+      <tr>
+        <td>Jane Smith</td>
+        <td>25</td>
+        <td>Los Angeles</td>
+      </tr>
+      <tr>
+        <td>Sam Brown</td>
+        <td>22</td>
+        <td>Chicago</td>
+      </tr>
+    </tbody>
+  </table>
+)
 
 const containerVideo = document.getElementById('video');
 const video = createRoot(containerVideo);
