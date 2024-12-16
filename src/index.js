24f7fe54272f69f8521162831753330cd4ad862e
whispfear <whispfear@gmail.com>
2024-12-16 10:39:43 +0200

    render_complex41241

--- src/index.js
+++ src/index.js
@@ -5,20 +5,10 @@ import App from './App';
 import Test from './test';
 import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = createRoot(document.getElementById("root"));
+root.render (<App />);
 
-const containerTest = document.getElementById("test");
-const test = createRoot(containerTest);
-test.render(
-  <React.StrictMode>
-    <Test />
-  </React.StrictMode>
-)
+const test = createRoot(document.getElementById("test"));
+test.render (<Test />);
 
 reportWebVitals();
